fe60562b45748c0729c1193db8fc58e030d12ea8
always <alwaysmeorg@gmail.com>
2026-09-03 10:23:12 +0800

    update: 更新vite配置

--- vite.config.ts
+++ vite.config.ts
@@ -8,19 +8,18 @@ const port: number = Number(process.env.TAURI_DEV_PORT || 3000)
 export default defineConfig(async () => ({
 	plugins: [vue()],
 
-	// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-	//
-	// 1. prevent Vite from obscuring rust errors
+	// 防止 Vite 清除 Rust 显示的错误
 	clearScreen: false,
-	// 2. tauri expects a fixed port, fail if that port is not available
 	server: {
 		port,
 		strictPort: true,
 		host: host,
 		hmr: { protocol: 'ws', host, port },
 		watch: {
-			// 3. tell Vite to ignore watching `src-tauri`
+			// Vite 忽略监听 `src-tauri` 目录
 			ignored: ['**/src-tauri/**'],
 		},
 	},
+	// 添加有关当前构建目标的额外前缀，使这些 CLI 设置的 Tauri 环境变量可以在客户端代码中访问
+	envPrefix: ['VITE_', 'TAURI_ENV_*'],
 }))
